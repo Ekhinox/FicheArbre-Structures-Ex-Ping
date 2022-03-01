5764ca510d67ab9ee97805a0a5b4b36b3aa6db79
Camille <folcamille@yahoo.fr>
2022-03-01 16:55:48 +0100

    avancé

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import fetch from 'node-fetch';
-let url = 'https://wikipedia.org';
+let urlz = ['https://wikipedia.org','http://google.com','http://google.com','http://google.com','http://google.com'];
+
 async function testUrl(url){
     let start = new Date();
     let response = await fetch(url)
     let end = new Date();
-    //    return end;
      console.log('ça marche');
-     console.log(`${url} a mis ${end - start} ms à s'afficher`)
+     return(`${url} a mis ${end - start} ms à s'afficher`)
 }
-testUrl(url);
+
+//let test = [];
+
+//option 1 avec let test
+//urlz.forEach(url => test.push(testUrl(url)));
+
+//option 2 avec let test
+/*urlz.forEach(async (url) => {
+    const result = testUrl(url);
+    test.push(result)
+})*/
+//console.log(test);
+
+//Option 3 avec let test2 et .map 
+let test2 = urlz.map(url => testUrl(url)); 
+
+
+async function requestUrl(){
+    let allPromise = await Promise.all(test2) //ou (test) si option 1 et 2
+    return allPromise;
+}
+
+let resultat = await requestUrl();
+console.log(resultat);
